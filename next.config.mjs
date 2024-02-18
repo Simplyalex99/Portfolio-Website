41ec741736412afef6e5cf47304d1a33358481d0
Simplyalex99 <alexmflores99@hotmail.ca>
2024-02-18 17:07:11 -0500

    add: PWA set-up

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPlaiceholder from "@plaiceholder/next";
+import withPWA from "next-pwa";
+import withYAML from "next-yaml";
 
-export default nextConfig;
+const nextConfig = {};
+const withPWA_ = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+export default withPlaiceholder(withYAML({ ...withPWA_, ...nextConfig }));
