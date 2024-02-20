612f0982b423f9d2239b4e065f9a755274dd04e4
Simplyalex99 <alexmflores99@hotmail.ca>
2024-02-20 16:08:37 -0500

    add: common components

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import FooterStyles from '@/styles/components/Footer.module.scss';
+
+import { Links } from '../../enums/links';
+
+export const Footer = () => {
+  const { HOME_PATH, CONTACT_PATH, GITHUB_LINK } = Links;
+  return (
+    <footer
+      className={`${FooterStyles.footer} ${FooterStyles['footer-bg']}
+}`}
+    >
+      <div className={FooterStyles['footer-wrapper']}>
+        <div
+          className={`${FooterStyles['footer-links']}
+          `}
+        >
+          <Link href={HOME_PATH}>
+            <p className={FooterStyles['footer-link']}>Home</p>
+          </Link>
+          <Link href={CONTACT_PATH}>
+            <p className={FooterStyles['footer-link']}>Contact</p>
+          </Link>
+          <Link href={GITHUB_LINK}>
+            <p className={FooterStyles['footer-link']}>GitHub</p>
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,118 @@
+'use client';
+
+import React, { useState } from 'react';
+import Link, { LinkProps } from 'next/link';
+import { Links } from '@/enums';
+import navbarStyles from '@/styles/components/Navbar.module.scss';
+import { HamburgerMenuSVG } from '../svg/common/HamburgerMenu';
+import { CloseSVG } from '../svg/common/Close';
+import { useToggleNavbarMenu } from '../../hooks/index';
+
+export type NavItemProps = {
+  href: string;
+  children: React.ReactNode;
+};
+export const NavItem = (props: NavItemProps & LinkProps) => {
+  const { children } = props;
+  return <Link {...props}>{children}</Link>;
+};
+
+export enum ActiveTabType {
+  HOME = 'HOME',
+  CONTACT = 'CONTACT',
+}
+
+export const Navbar = () => {
+  const [activeTab, setActiveTab] = useState(ActiveTabType.HOME);
+  const getIsActive = (type: ActiveTabType) => {
+    return activeTab === type ? navbarStyles['active-tab'] : '';
+  };
+  const { HOME_PATH } = Links;
+
+  const toggleProps = {
+    menuId: 'menu-wrapper',
+    closeId: 'close',
+    navbarId: 'nav',
+    className: navbarStyles['open-nav'],
+  };
+  useToggleNavbarMenu(toggleProps);
+  return (
+    <div
+      id="navbar"
+      className={`${navbarStyles['drop-shadow']} ${navbarStyles['navbar-bg']}`}
+    >
+      <header className={`flex  wrapper ${navbarStyles.container}`}>
+        <div className={`flex ${navbarStyles['custom-link-wrapper']}`}>
+          <div
+            id="menu-wrapper"
+            role="presentation"
+            className={`
+                ${navbarStyles['menu-wrapper']}
+            }`}
+          >
+            <HamburgerMenuSVG
+              className={`${navbarStyles['menu-icon']}  `}
+              width="35"
+              height="35"
+            />
+          </div>
+
+          <Link href={HOME_PATH}>
+            <button type="button" className={`${navbarStyles['custom-link']}`}>
+              MyPortfolio
+            </button>
+          </Link>
+        </div>
+        <nav
+          id="nav"
+          className={`${navbarStyles['nav-container']}
+
+              `}
+        >
+          <div
+            role="presentation"
+            id="close"
+            className={navbarStyles['close-icon-wrapper']}
+          >
+            <CloseSVG
+              className={`${navbarStyles['close-icon']} ${navbarStyles['close-icon-light']}`}
+            />
+          </div>
+          <div className={`${navbarStyles['links-wrapper']}`}>
+            <Link href={HOME_PATH}>
+              <div
+                role="presentation"
+                onClick={() => setActiveTab(ActiveTabType.HOME)}
+                className={`${navbarStyles['nav-item-wrapper']}`}
+              >
+                <button
+                  type="button"
+                  className={`${getIsActive(ActiveTabType.HOME)} ${
+                    navbarStyles['nav-item']
+                  }  `}
+                >
+                  <span>{ActiveTabType.HOME}</span>
+                </button>
+              </div>
+            </Link>
+            <Link href={Links.CONTACT_PATH}>
+              <div
+                role="presentation"
+                className={`${navbarStyles['nav-item-wrapper']}`}
+                onClick={() => setActiveTab(ActiveTabType.CONTACT)}
+              >
+                <button
+                  type="button"
+                  className={`${navbarStyles['nav-item']} ${navbarStyles.contact}`}
+                >
+                  <span> {ActiveTabType.CONTACT}</span>
+                </button>
+              </div>
+            </Link>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+export default Navbar;
