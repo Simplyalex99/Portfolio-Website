81982e900d5a70fb4bcda86fc3e7795836180a71
Simplyalex99 <alexmflores99@hotmail.ca>
2024-06-21 11:45:39 -0400

    update: right arrow

--- src/components/svg/arrows/RightArrow.tsx
+++ src/components/svg/arrows/RightArrow.tsx
@@ -5,14 +5,17 @@ export const RightArrowSVG: React.FC<React.SVGProps<SVGSVGElement>> = (
 ) => {
   return (
     <svg
-      width="113"
-      height="8"
-      viewBox="0 0 113 2"
+      width="100"
+      height="10"
+      viewBox="0 0 100 10"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M0 1L112.004 1" stroke="#666666" />
+      <path
+        d="M100 5L92.5 0.669873V9.33013L100 5ZM0 5.75H93.25V4.25H0V5.75Z"
+        fill="#8C8C8C"
+      />
     </svg>
   );
 };
