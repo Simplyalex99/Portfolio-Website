5639a2ed02bd8a4b4df0073942837757c75bef79
Simplyalex99 <alexmflores99@hotmail.ca>
2024-10-03 11:50:00 -0400

    styles: updated font size and projects

--- src/components/framer/HorizontalScroll.tsx
+++ src/components/framer/HorizontalScroll.tsx
@@ -6,15 +6,17 @@ import { loadFeatures } from '@/helpers';
 import RightArrowSVG from '../svg/arrows/RightArrow';
 export const HorizontalScroll = ({
   children,
+  className,
 }: {
   children: React.ReactNode;
+  className?: string;
 }) => {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({ target: ref });
   const x = useTransform(scrollYProgress, [0, 1], ['0%', '-95%']);
 
   return (
-    <div className={styles.container} ref={ref}>
+    <div className={`${styles.container} ${className}`} ref={ref}>
       <div className={styles.accordian}>
         <div className={styles['text-container']}>
           <p className={styles.text}>Scroll</p>
